8a6b749cd8fa79a3d1eb5bdda8b4ebabc11bba7f
Mujahid Ali <itxmujahidali@gmail.com>
2021-09-08 05:54:56 -0700

    Weather API created and render data to HTML page. Created Database Tables.
    1. In this branch, I created weather API and render data to html page.
    2. I also created Database tables.
    ------ Database Table Name ------
    1. WebUser
    2. Plant
    3. Sensor
    4. WeatherAPI
    5. ScheduleWater

--- Smart_Irrigation_System/urls.py
+++ Smart_Irrigation_System/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login', include('SIS_APP.urls')),
     path('register', include('SIS_APP.urls')),
     path('settings', include('SIS_APP.urls')),
+    path('weather', include('SIS_APP.urls')),
     path('error404', include('SIS_APP.urls')),
     path('error500', include('SIS_APP.urls')),
 ]
