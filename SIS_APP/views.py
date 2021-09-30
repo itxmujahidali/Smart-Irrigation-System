56e750b5439b8f3f65f6c4641db42d527d375e03
Mujahid Ali <46293412+itxmujahidali@users.noreply.github.com>
2021-09-30 14:26:38 -0700

    saving value into csv fie

--- SIS_APP/views.py
+++ SIS_APP/views.py
@@ -1,4 +1,5 @@
-import json
+import json, csv, os.path
+from datetime import datetime
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
@@ -75,8 +76,22 @@ def settings(request):
     id = request.session.get('id')
     user =  WebUser.objects.get(user_id=id)
     user_name = user.name
+    farm_name = user.farm_name
     user_email = user.email
-    return render(request, 'account_settings.html',{"user_name":user_name,'user_email':user_email})
+    user_contact = user.contact
+    if(request.method == "POST"):
+        farm_name = request.POST['farmName']
+        user_name = request.POST['name']
+        user_email = request.POST['email']
+        user_contact = request.POST['contact']
+        User = WebUser(farm_name=farm_name, user_name=user_name, email=user_email, user_contact=user_contact)
+        User.save()
+        return render(request, 'account_settings.html',
+            {"user_name":user_name,'user_email':user_email,"farm_name":farm_name,"user_contact":user_contact})
+    else:
+        return render(request, 'account_settings.html',
+            {"user_name":user_name,'user_email':user_email,"farm_name":farm_name,"user_contact":user_contact})
+
 
 def logout(request):
     request.session.flush()
@@ -84,18 +99,50 @@ def logout(request):
 
 @csrf_exempt
 def sensor(request):
+
+    from datetime import datetime
+
     # Only listen to PUT requests
     # if request.method != "PUT" or request.body == None:
     #     response_message = "Only PUT requests are allowed on this route."
     #     response_code = 405
     #     return HttpResponse(response_message,response_code)
     # else:
+    
     # Load body
     body = json.loads(request.body)
-    print (body["email"])
-    print (body["password"])
-    print (body["positionX"])
-    print (body["current_moisture"])
+
+    # datetime object containing current date and time
+    now = datetime.now()
+
+    print (body["sensor_name"])
+    sensor_name = (body["sensor_name"])
+
+    print (body["reading"])
+    reading = (body["reading"])
+
+    # print (body["positionX"])
+    # positionX = (body["positionX"])
+
+    # print (body["current_moisture"])
+    # current_moisture = (body["current_moisture"])
+
+    # datetime object containing current date and time
+    now = datetime.now()
+    date = now.strftime("%d/%m/%Y")
+    time = now.strftime("%H:%M:%S")
+    print("date and time =", date, time)
+
+    if (os.path.isfile("data.csv")):  
+        with open('data.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([sensor_name, reading," ", date, time])
+            writer.writerow([sensor_name, reading," ", date, time])
+    else:
+         with open('data.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([sensor_name, reading," ", date, time])
+
     return HttpResponse ("Success!")
 
     # if 'email' not in body or 'positionX' not in body or 'positionY' not in body\
