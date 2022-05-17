4d6beeaf5c5fb2b620c2ed57c5a96d22e9752dd6
Mujahid Ali <46293412+itxmujahidali@users.noreply.github.com>
2022-05-17 12:53:37 -0700

    forget pages/chart.js
    in this commit i add forget pages (static mode) and add chart.js (static mode).

--- SIS_APP/views.py
+++ SIS_APP/views.py
@@ -11,85 +11,86 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def index(request):
-    if request.session.has_key('id'):
-        id = request.session.get('id')
-        user = WebUser.objects.get(user_id=id)
-
-        
     try:
-        id = request.session['id'] #getting id by session
-        info = WeatherAPI.objects.filter(FK_weather = id)
-        CITY = info[0].city
+        if request.session.has_key('id'):
+            id = request.session.get('id')
+            user = WebUser.objects.get(user_id=id)
+
+            
+        try:
+            id = request.session['id'] #getting id by session
+            info = WeatherAPI.objects.filter(FK_weather = id)
+            CITY = info[0].city
+        except:
+            return HttpResponse ("City Error!")
+
+
+        # Your API key
+        API_KEY = "84b2833cfdc8bca61f826270f0c4dc55"
+
+        # updating the URL
+        URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
+
+        # Sending HTTP request
+        response = requests.get(URL)
+
+        # checking the status code of the request
+        if response.status_code == 200:
+            
+            # retrieving data in the json format
+            data = response.json()
+            
+            # take the main dict block
+            main = data['main']
+            
+            # getting temperature
+            temperature = main['temp']
+            temperature_celsius = temperature - 273.15
+            # getting feel like
+            temp_feel_like = main['feels_like']
+            temp_feel_like_celsius = temp_feel_like - 273.15
+            # getting the humidity
+            humidity = main['humidity']
+            # getting the pressure
+            # pressure = main['pressure']
+            
+            # weather report
+            weather_report = data['weather']
+            # wind report
+            wind_report = data['wind']
+            
+            CITY = CITY.upper() #All Uppercase Letter of City
+
+            dic_tempreature =  int(temperature_celsius)
+            dic_feels_like =  int(temp_feel_like_celsius)
+            dic_humidity =  humidity
+            dic_description =  weather_report[0]['description']
+            dic_description = dic_description.capitalize() #Capitalize First Letter
+            dic_speed =  wind_report['speed']
+            dic_timezone =  data['timezone']
+
+            #Save value in database
+            # webuser = WebUser.objects.get(user_id=5)
+            # api_database = WeatherAPI(FK_weather=webuser,city=CITY, city_id=dic_id, temperature=dic_tempreature, feels_like=dic_feels_like, 
+            # humidity=dic_humidity, weather_report=dic_description, wind_speed=dic_speed, time_zone=dic_timezone)
+            # api_database.save()
+
+            return render (request, 'index.html', {
+                'name': user.name,
+                'CITY': CITY,
+                'dic_tempreature': dic_tempreature,
+                'dic_feels_like': dic_feels_like,
+                'dic_humidity': dic_humidity,
+                'dic_description': dic_description,
+                'dic_speed': dic_speed,
+                'dic_timezone': dic_timezone,}
+                )
+
+        else:
+            # showing the error message
+            return HttpResponse ("Error in the HTTP request")
     except:
-        return HttpResponse ("City Error!")
-
-
-    # Your API key
-    API_KEY = "84b2833cfdc8bca61f826270f0c4dc55"
-
-    # updating the URL
-    URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
-
-    # Sending HTTP request
-    response = requests.get(URL)
-
-    # checking the status code of the request
-    if response.status_code == 200:
-        
-        # retrieving data in the json format
-        data = response.json()
-        
-        # take the main dict block
-        main = data['main']
-        
-        # getting temperature
-        temperature = main['temp']
-        temperature_celsius = temperature - 273.15
-        # getting feel like
-        temp_feel_like = main['feels_like']
-        temp_feel_like_celsius = temp_feel_like - 273.15
-        # getting the humidity
-        humidity = main['humidity']
-        # getting the pressure
-        # pressure = main['pressure']
-        
-        # weather report
-        weather_report = data['weather']
-        # wind report
-        wind_report = data['wind']
-        
-        CITY = CITY.upper() #All Uppercase Letter of City
-
-        dic_tempreature =  int(temperature_celsius)
-        dic_feels_like =  int(temp_feel_like_celsius)
-        dic_humidity =  humidity
-        dic_description =  weather_report[0]['description']
-        dic_description = dic_description.capitalize() #Capitalize First Letter
-        dic_speed =  wind_report['speed']
-        dic_timezone =  data['timezone']
-
-        #Save value in database
-        # webuser = WebUser.objects.get(user_id=5)
-        # api_database = WeatherAPI(FK_weather=webuser,city=CITY, city_id=dic_id, temperature=dic_tempreature, feels_like=dic_feels_like, 
-        # humidity=dic_humidity, weather_report=dic_description, wind_speed=dic_speed, time_zone=dic_timezone)
-        # api_database.save()
-
-        return render (request, 'index.html', {
-            'name': user.name,
-            'CITY': CITY,
-            'dic_tempreature': dic_tempreature,
-            'dic_feels_like': dic_feels_like,
-            'dic_humidity': dic_humidity,
-            'dic_description': dic_description,
-            'dic_speed': dic_speed,
-            'dic_timezone': dic_timezone,}
-            )
-
-    else:
-        # showing the error message
-        return HttpResponse ("Error in the HTTP request")
-
-
+            return render (request, "index.html")
     ##################################################################################################
 
     # API Credentials
@@ -180,6 +181,24 @@ def settings(request):
         return render(request, 'account_settings.html',
             {"user_name":user_name,'user_email':user_email,"farm_name":farm_name,"user_contact":user_contact})
 
+
+def statics(request):
+    return render(request, 'statics.html')
+
+def addsensors(request):
+    return render(request, 'addsensors.html')
+
+def forgetpassword1(request):
+    return render(request, 'forgetpassword1.html')
+
+def forgetpassword2(request):
+    return render(request, 'forgetpassword2.html')
+
+def forgetpassword3(request):
+    return render(request, 'forgetpassword3.html')
+
+def forgetpassword4(request):
+    return render(request, 'forgetpassword4.html')
 
 def logout(request):
     request.session.flush()
